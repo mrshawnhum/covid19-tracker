b3602cfd2c4508e4f17d2d9c1a7f5d729856abaa
Shawn Humphreys <shawnhum97@gmail.com>
2020-07-09 15:15:07 -0500

    fetch Daily Data

--- src/api/index.js
+++ src/api/index.js
@@ -5,7 +5,7 @@ const url = 'https://covid19.mathdro.id/api'
 export const fetchData = async () => {
     try {
     const { data: { confirmed, recovered, deaths, lastUpdate }} = await axios.get(url)
-     
+
     return {
         confirmed,
         recovered,
@@ -14,5 +14,20 @@ export const fetchData = async () => {
     }
     } catch (e) {
     console.log(e)
+    }
+}
+
+export const fetchDailyData = async () => {
+    try {
+        const {data} = await axios.get(`${url}/daily`)
+        const modifiedData = data.map((dailyData) => ({
+            confirmed: dailyData.confirmed.total,
+            deaths: dailyData.deaths.total,
+            date: dailyData.reportDate
+        }));
+
+        return modifiedData;
+    } catch (error) {
+        console.log(error)
     }
 }
