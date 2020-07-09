15c4aa434da1b4f3d73a23fc71cb576708043061
Shawn Humphreys <shawnhum97@gmail.com>
2020-07-09 15:17:17 -0500

    Restructure card

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import {Card, CardContent, Typography, Grid} from '@material-ui/core'
+import Countup from 'react-countup'
+import cx from 'classnames'
 
 import styles from './Cards.module.css'
 
-export const Cards = (p) => {
-    console.log(p);
-
+export const Cards = ({data: {confirmed, recovered, deaths, lastUpdate}}) => {
+    if(!confirmed) {
+        return 'Loading...'
+    }
     return (
     <div className={styles.container}>
         <Grid container spacing={3} justify='center'>
